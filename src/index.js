7ad19d5fe0c0020b6668a0567fd9bc25c0346910
Taher <ttaherhagui@gmail.com>
2021-10-28 00:41:25 +0100

    Add Required features for interactive lists.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './style.css';
 import RETURN from './assets/Images/return.png';
 import REFRESH from './assets/Images/refresh.png';
 import THREE from './assets/Images/three-points.png';
+import * as status from './status.js';
 /* declarate variables */
-const toDoTasks = [
+let toDoTasks = [
   {
     description: 'working on my capstone project',
     completed: false,
@@ -25,18 +26,41 @@ const toDoTasks = [
 const placeholder = document.querySelector('.container');
 let nbrGrid = 2;
 const clearButton = document.createElement('div');
+/* local storage */ 
+if (!localStorage.getItem('toDoTasks')) {
+  localStorage.setItem('toDoTasks',JSON.stringify(toDoTasks));
+}
+else {
+    toDoTasks = JSON.parse(localStorage.getItem('toDoTasks'));
+}
 /* utilities */
 const populateTask = (tasks) => {
   tasks.forEach((element) => {
     const newElement = document.createElement('div');
-    newElement.innerHTML = `<div><input type='checkbox' class= 'selection-icon'> <p class='task-description'> ${element.description}</p> </div><img src=${THREE} class='edit'>`;
+    if (!element.completed) {
+      newElement.innerHTML = `<div><input type='checkbox' class= 'selection-icon'> <p class='task-description'> ${element.description}</p> </div><img src=${THREE} class='edit'>`;
+    }
+    else {
+        newElement.innerHTML = `<div><input type='checkbox' class= 'selection-icon' checked> <p class='task-description'> ${element.description}</p> </div><img src=${THREE} class='edit'>`; 
+    }
     newElement.style.gridRow = `${element.index + 2} / span 1`;
     newElement.className = 'task-card';
     nbrGrid += 1;
     placeholder.appendChild(newElement);
+    const checkboxInput = newElement.querySelector('.selection-icon');
+    checkboxInput.addEventListener('change', () => {
+      if (element.completed) {
+        element.completed = false;
+        localStorage.setItem('toDoTasks',JSON.stringify(toDoTasks));
+        }
+      else {
+        element.completed = true;
+        localStorage.setItem('toDoTasks',JSON.stringify(toDoTasks));
+      }
+    });
+    placeholder.style.gridTemplateRows = `${nbrGrid}`;
   });
-  placeholder.style.gridTemplateRows = `${nbrGrid}`;
-};
+}
 /* Load Page */
 const titleList = document.createElement('div');
 titleList.innerHTML = `<p class='description'>Today's To Do </p> <img src=${REFRESH} class='icon'>`;
@@ -55,3 +79,4 @@ nbrGrid += 1;
 clearButton.style.gridRow = `${nbrGrid} / span 1`;
 placeholder.style.gridTemplateRows = nbrGrid;
 placeholder.appendChild(clearButton);
+
