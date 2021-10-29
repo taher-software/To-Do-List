850f48e67808990a7aef51d81df5c31523a51870
Taher <ttaherhagui@gmail.com>
2021-10-30 00:48:38 +0100

    Add required features.

---
+++ src/add_remove.js
@@ -0,0 +1,131 @@
+import THREE from './assets/Images/three-points.png';
+import DEL from './assets/Images/delete.png';
+import change from './status.js';
+
+const addTask = (toDoTasks, taskdescription, nbrGrid, eddList) => {
+  const placeholder = document.querySelector('.container');
+  const newIndex = toDoTasks.length + 1;
+  const newElt = document.createElement('div');
+  newElt.innerHTML = `<div><input type='checkbox' class= 'selection-icon'> <p class='task-description'> ${taskdescription}</p> </div><img src=${THREE} class='edit'>`;
+  newElt.className = 'task-card';
+  newElt.style.gridRow = `${newIndex + 2} / span 1`;
+  document.querySelector('.clear-button').style.gridRow = `${newIndex + 3} / span 1`;
+  const nbrOfGrid = nbrGrid + 1;
+  placeholder.appendChild(newElt);
+  placeholder.style.gridTemplateRows = `repeat(${nbrOfGrid}, 1fr)`;
+  toDoTasks.push({
+    description: taskdescription,
+    completed: false,
+    index: newIndex,
+  });
+  eddList.push(newElt.querySelector('.edit'));
+
+  /* newElt.querySelector('.edit').onclick = (e) => {
+        [nbrOfGrid,deList] = editTask(e,toDoTasks,nbrOfGrid,deList);
+    } */
+  newElt.querySelector('.selection-icon').onchange = (e) => {
+    change(e, toDoTasks);
+  };
+  return [nbrOfGrid, eddList, toDoTasks];
+};
+const clickTask = (e, toDoTasks, nbrGrid, eddList) => {
+  /* const toDoTasks = JSON.parse(localStorage.getItem('toDoTasks')); */
+  const taskdescription = e.target.value;
+  e.target.value = '';
+  const [nbrOfGrid, edd, newList] = addTask(toDoTasks, taskdescription, nbrGrid, eddList);
+  localStorage.setItem('toDoTasks', JSON.stringify(newList));
+  return [nbrOfGrid, edd];
+};
+const deleteTask = (e, taskList, nbrGrid) => {
+  const elt = e.target.parentNode;
+  const parent = elt.parentNode;
+  const indexElt = parseInt(getComputedStyle(elt).gridRow.split('/')[0], 10) - 2;
+  parent.removeChild(elt);
+  let ind = -1;
+  for (let i = 0; i < taskList.length; i += 1) {
+    if (taskList[i].index === indexElt) {
+      ind = i;
+      break;
+    }
+  }
+  taskList.splice(ind, 1);
+  const nbrOfGrid = nbrGrid - 1;
+  parent.style.gridTemplateRows = `repeat(${nbrOfGrid}, 1fr)`;
+  for (let i = 0; i < taskList.length; i += 1) {
+    if (taskList[i].index > indexElt) {
+      taskList[i].index -= 1;
+    }
+  }
+  localStorage.setItem('toDoTasks', JSON.stringify(taskList));
+  Array.from(parent.children).forEach((element) => {
+    if (element.className === 'task-card') {
+      const gridOrder = parseInt(getComputedStyle(element).gridRow.split('/')[0], 10);
+      if (gridOrder > indexElt + 2) {
+        element.style.gridRow = `${gridOrder - 1} / span 1`;
+      }
+    }
+  });
+  const btn = document.querySelector('.clear-button');
+  btn.style.gridRow = `${parseInt(getComputedStyle(btn).gridRow.split('/')[0], 10) - 1} / span 1`;
+
+  return nbrOfGrid;
+};
+const editTask = (e, taskList, nbr) => {
+  const elt = e.target.parentNode;
+  const basicContent = elt.innerHTML;
+  const nbrOfGrid = nbr;
+  elt.innerHTML = `<div><input type='checkbox' class= 'selection-icon'> <input type='text' class='edit-task'> </div><img src=${DEL} class='del'>`;
+  elt.style.backgroundColor = 'rgba(228, 214, 151,0.8)';
+  const removeElem = [elt.querySelector('.del')];
+  const inputElem = elt.querySelector('.edit-task');
+  inputElem.onchange = () => {
+    const newText = inputElem.value;
+    const elt = inputElem.parentNode.parentNode;
+    const index = parseInt(getComputedStyle(elt).gridRow.split('/')[0], 10) - 2;
+    elt.innerHTML = basicContent;
+    elt.querySelector('.task-description').textContent = newText;
+    elt.style.backgroundColor = '#fff';
+    elt.querySelector('.selection-icon').onchange = (e) => {
+      change(e, taskList);
+    };
+    taskList[index - 1].description = newText;
+    localStorage.setItem('toDoTasks', JSON.stringify(taskList));
+  };
+  return [nbrOfGrid, removeElem];
+};
+const clear = (storageFct) => {
+  const taskList = JSON.parse(localStorage.getItem('toDoTasks'));
+  const container = document.querySelector('.container');
+  const childrens = Array.from(container.children);
+  const firstLength = taskList.length;
+  for (let i = 0; i < childrens.length; i += 1) {
+    if (childrens[i].className === 'task-card') container.removeChild(childrens[i]);
+  }
+  const newTaskList = taskList.filter((task) => task.completed === false);
+  const removedIndex = Array.apply(0, Array(firstLength)).map((_, b) => b + 1);
+
+  newTaskList.forEach((el) => {
+    const pos = removedIndex.indexOf(el.index);
+    removedIndex.splice(pos, 1);
+  });
+  if (removedIndex.length > 0) {
+    for (let i = removedIndex.length - 1; i >= 0; i -= 1) {
+      newTaskList.forEach((el) => {
+        if (el.index > removedIndex[i]) {
+          el.index -= 1;
+        }
+      });
+    }
+  }
+  storageFct(newTaskList, container);
+  const containerNbrGrid = newTaskList.length + 3;
+  container.style.gridTemplateRows = `repeat(${containerNbrGrid},1fr)`;
+  const btn = container.querySelector('.clear-button');
+  btn.style.gridRow = `${containerNbrGrid} / span 1`;
+  localStorage.setItem('toDoTasks', JSON.stringify(newTaskList));
+
+  return containerNbrGrid;
+};
+export {
+  addTask, clickTask, editTask, deleteTask, clear,
+};
--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 /* import models */
 import './style.css';
-/* import REFRESH from './assets/Images/refresh.svg'; */
 import RETURN from './assets/Images/return.png';
 import REFRESH from './assets/Images/refresh.png';
 import THREE from './assets/Images/three-points.png';
 import change from './status.js';
+import * as CRUD from './add_remove.js';
 /* declarate variables */
-let toDoTasks = [
-  {
-    description: 'working on my capstone project',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'do my daily workout',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Visit my parents',
-    completed: false,
-    index: 2,
-  },
-];
+let toDoTasks = [];
 const placeholder = document.querySelector('.container');
 let nbrGrid = 2;
 const clearButton = document.createElement('div');
+let removedListListner = [];
 /* local storage */
 if (!localStorage.getItem('toDoTasks')) {
   localStorage.setItem('toDoTasks', JSON.stringify(toDoTasks));
@@ -33,13 +18,21 @@ if (!localStorage.getItem('toDoTasks')) {
   toDoTasks = JSON.parse(localStorage.getItem('toDoTasks'));
 }
 /* utilities */
-const populateTask = (tasks) => {
+const populateTask = (tasks, placeholder) => {
+  nbrGrid = 2;
   tasks.forEach((element) => {
     const newElement = document.createElement('div');
     if (!element.completed) {
       newElement.innerHTML = `<div><input type='checkbox' class= 'selection-icon'> <p class='task-description'> ${element.description}</p> </div><img src=${THREE} class='edit'>`;
+      const textElt = newElement.querySelector('.task-description');
+      textElt.style.textDecoration = 'none';
+      textElt.style.textDecorationColor = '#fff';
     } else {
       newElement.innerHTML = `<div><input type='checkbox' class= 'selection-icon' checked> <p class='task-description'> ${element.description}</p> </div><img src=${THREE} class='edit'>`;
+      const textElt = newElement.querySelector('.task-description');
+      textElt.style.textDecoration = 'line-through';
+      textElt.style.textDecorationColor = 'rgb(0, 0, 255)';
+      textElt.style.textDecorationThickness = '3px';
     }
     newElement.style.gridRow = `${element.index + 2} / span 1`;
     newElement.className = 'task-card';
@@ -49,7 +42,7 @@ const populateTask = (tasks) => {
     checkboxInput.addEventListener('change', (e) => {
       change(e, toDoTasks);
     });
-    placeholder.style.gridTemplateRows = `${nbrGrid}`;
+    placeholder.style.gridTemplateRows = `repeat(${nbrGrid}, 1fr)`;
   });
 };
 /* Load Page */
@@ -63,10 +56,37 @@ addTask.className = 'fix-card';
 addTask.style.gridRow = '2 / span 1';
 placeholder.appendChild(titleList);
 placeholder.appendChild(addTask);
-populateTask(toDoTasks);
+populateTask(toDoTasks, placeholder);
 clearButton.innerHTML = '<p>Clear all completed</p>';
 clearButton.className = 'clear-button';
 nbrGrid += 1;
 clearButton.style.gridRow = `${nbrGrid} / span 1`;
-placeholder.style.gridTemplateRows = nbrGrid;
+placeholder.style.gridTemplateRows = `repeat(${nbrGrid}, 1fr)`;
 placeholder.appendChild(clearButton);
+/* track add task changes */
+let edditList = Array.from(document.querySelectorAll('.edit'));
+document.querySelector('.fix-card input').addEventListener('change', (e) => {
+  [nbrGrid, edditList] = CRUD.clickTask(e, toDoTasks, nbrGrid, edditList);
+  edditList.forEach((ev) => {
+    ev.addEventListener('click', (el) => {
+      [nbrGrid, removedListListner] = CRUD.editTask(el, toDoTasks, nbrGrid);
+      removedListListner[0].onclick = (cl) => {
+        nbrGrid = CRUD.deleteTask(cl, toDoTasks, nbrGrid);
+      };
+    });
+  });
+});
+/* track edit icons */
+edditList.forEach((e) => {
+  e.addEventListener('click', (el) => {
+    [nbrGrid, removedListListner] = CRUD.editTask(el, toDoTasks, nbrGrid);
+    removedListListner[0].onclick = (cl) => {
+      nbrGrid = CRUD.deleteTask(cl, toDoTasks, nbrGrid);
+    };
+  });
+});
+/* track clear completed Button */
+const btn = document.querySelector('.clear-button');
+btn.addEventListener('click', () => {
+  nbrGrid = CRUD.clear(populateTask);
+});
